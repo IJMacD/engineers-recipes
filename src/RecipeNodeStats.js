9b7784ed9cd0ffe66f967d7fbea086a520a944e8
Iain MacDonald <ijmacd@gmail.com>
2022-02-15 00:50:14 +0800

    Update stats

--- src/RecipeNodeStats.js
+++ src/RecipeNodeStats.js
@@ -6,9 +6,15 @@
  */
 export function RecipeNodeStats({ node }) {
     const actionCount = countActions(node);
+    const completeCount = countCompleteActions(node);
 
     return (
-        <p>Total Actions: {actionCount}</p>
+        <div>
+            <p>Total Actions: {actionCount}</p>
+            <p>Complete Actions: {completeCount}</p>
+            <p>Incomplete Actions: {actionCount - completeCount}</p>
+            <p>Completed: {(completeCount/actionCount*100).toFixed()}%</p>
+        </div>
     );
 }
 
@@ -16,6 +22,13 @@ export function RecipeNodeStats({ node }) {
  * @param {import("./types").RecipeNode} node
  */
 function countActions (node) {
-    const baseCount = typeof node.action === "string" ? 1 : node.action.length;
-    return baseCount + node.pre.reduce((sum, p) => sum + (typeof p === "string" ? 0 : countActions(p)), 0);
+    return 1 + node.pre.reduce((sum, p) => sum + (typeof p === "string" ? 0 : countActions(p)), 0);
+}
+
+/**
+ * @param {import("./types").RecipeNode} node
+ */
+function countCompleteActions (node) {
+    const baseCount = node.completed ? 1 : 0;
+    return baseCount + node.pre.reduce((sum, p) => sum + (typeof p === "string" ? 0 : countCompleteActions(p)), 0);
 }
